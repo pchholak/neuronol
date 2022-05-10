14c86d928ed1b610c3ad6b87ddf94d3b54b5d426
Parth Chholak <creator@mastertape.lan>
2022-05-10 14:34:57 -0400

    initial upload

---
+++ AP/neuronol_power.py
@@ -0,0 +1,490 @@
+import mne
+import pickle
+import numpy as np
+import os.path as op
+import matplotlib.pyplot as plt
+from scipy import signal
+
+plt.rcParams['figure.figsize'] = [8, 5]
+plt.rcParams['font.size'] = 14
+
+def featureFunc_bandAbsolutePower(psds, freqs, band, channels=None, norm_and_log_density=None,
+                                  compute_magnitude=None, plot_psd=None):
+    '''
+    Calculate absolute power in a given frequency band for a given set of channels as used
+    by Bauer 2001. Optionally, normalize to bandwidth and log-transform absolute power density
+    as done by Rangaswamy et al. 2002.
+
+    The power spectral density are averaged over channels. Estimates of power within a
+    frequency band are estimated by integrating the averaged power spectral density over the
+    corresponding frequency band intervals.
+
+    Parameters
+    ----------
+    psds : 2D array
+        The power spectral densities of all channels [n_channels, n_freqs].
+    freqs : 1D array
+        Frequencies in Hertz.
+    band : tuple
+        The lowest and highest frequencies to integrate the power spectral densities over.
+    channels : list of int | None
+        Channel indices for channels to include (None, default, includes all).
+    norm_and_log_density : bool | None
+        Whether or not to normalize to bandwidth and log-transform absolute band power as done by
+        Rangaswamy et al. 2002. None, default, does not.
+    plot_psd: bool | None
+        Whether to plot the power spectral densities used for calculating the band absolute power.
+        None, default, does not.
+
+    Returns
+    -------
+    res : scalar
+        The integrated absolute power in the given frequency band.
+        [Optionally, if norm_and_log_dens == True] :-
+        The integrated absolute power in the given frequency band after normalization to
+        bandwidth and log-transformation.
+        [Optionally, if compute_magnitude == True] :-
+        The square root of the integrated absolute power in the given frequency band, either with
+        or without normalization to bandwidth and log-transformation.
+    '''
+    if channels is None:
+        mean_psd = np.mean(psds, axis=0)
+    elif len(channels) > 1:
+        mean_psd = np.mean(psds[channels, :], axis=0)
+    else:
+        mean_psd = psds[channels, :].reshape((-1,))
+
+    # Plot FFT
+    if plot_psd and plot_psd is not None:
+        plt.figure()
+        plt.plot(freqs, mean_psd, color='k', lw=2)
+        plt.xlabel('Frequency [Hz]')
+        plt.ylabel('Power Spectral Density [μV2/Hz]')
+        plt.tight_layout()
+        plt.show()
+
+    # Integrate PSD in the desired frequency band
+    res = _integrate_psd(mean_psd, freqs, band)
+
+    # Scale and log
+    if norm_and_log_density and norm_and_log_density is not None:
+        res = np.log(ap_band / (band[1] - band[0]))
+
+    # Compute frequency magnitude (in μV)
+    if compute_magnitude and compute_magnitude is not None:
+        res = np.sqrt(res)
+
+    return res
+
+def featureFunc_bandRelativePower(psds_mean_epochs, freqs, band_target, band_range,
+                                  channels=None, norm_and_log_density=None, plot_psd=None):
+    '''
+    Get absolute power (AP) measures using the desired AP implementation in the target frequency
+    band and the total frequency range to be studied. Subsequently, calculate relative power (RP)
+    of the target band.
+
+    Parameters
+    ----------
+    psds_mean_epochs : 2D array
+        Power spectral densities for all channels averaged over epochs
+        [n_channels, n_freqs].
+    freqs : 1D array
+        Frequencies in Hertz.
+    band_target : tuple
+        The lowest and highest frequencies of the target band.
+    band_target : tuple
+        The lowest and highest frequencies of the studied frequency range.
+    channels : list of int | None
+        Channel indices for channels to include (None, default, includes all).
+    norm_and_log_density : bool | None
+        Whether or not to normalize to bandwidth and log-transform absolute band power as done by
+        Rangaswamy et al. 2002. None, default, does not.
+    plot_psd: bool | None
+        Whether to plot the power spectral densities used for calculating the band absolute power.
+        None, default, does not.
+
+    Returns
+    -------
+    rp_target : scalar
+        The relative power in the given target frequency band.
+        [Optionally, if norm_and_log_dens == True] :-
+        The relative power in the given target frequency band after normalization to bandwidth
+        and log-transformation.
+    '''
+
+    ap_target = featureFunc_bandAbsolutePower(psds_mean_epochs, freqs, band_target,
+                                             channels=channels,
+                                             norm_and_log_density=norm_and_log_density,
+                                             plot_psd=plot_psd)
+    ap_range = featureFunc_bandAbsolutePower(psds_mean_epochs, freqs, band_range,
+                                            channels=channels,
+                                            norm_and_log_density=norm_and_log_density,
+                                            plot_psd=None)
+    return ap_target / ap_range
+
+def psd_M1(params, raw, plot_psd_epochs=None):
+    '''
+    Bauer2001
+    All EEG epochs are detrended and cosine tapered. EEG epochs with zero-
+    overlap are submitted to a Fast Fourier Transform which computes the power
+    spectral density (in μV2/Hz) of the EEG. The PSDs are averaged over epochs.
+
+    Parameters
+    ----------
+    params : dict
+        Chosen parameters to be used in calculations.
+    raw : MNE-Python Raw object
+        Raw file instance under study
+    plot_psd_epochs: bool | None
+        Whether to plot the power spectral densities for each epoch or not.
+        None, default, does not.
+
+    Returns
+    -------
+    psds_mean_epochs : 2D array
+        Power spectral densities for all channels averaged over epochs
+        [n_channels, n_freqs].
+    freqs : 1D array
+        Frequencies in Hertz.
+    '''
+    # Unpack parameters
+    T_max = params['T_max']
+    print('A total of %d seconds of raw EEG data has been studied.' % T_max)
+    Fs = params['sfreq']
+    print('Fs =', Fs)
+    T_ep = params['size_epoch']
+    print('Each epoch has %0.1f seconds of EEG data.' % T_ep)
+    times = params['epoch_times']
+    print('Shape of times =', np.shape(times))
+
+    # Get raw data
+    raw = raw.copy()
+    raw.del_proj(0) # delete Average EEG reference
+    raw_data = raw.get_data(picks='eeg')
+    raw_data = raw_data[:, :int(Fs * T_max)]
+    print('Shape of raw_data =', np.shape(raw_data))
+
+    # Data segmentation and PSD calculation
+    n_epochs = int(np.floor(T_max / T_ep))
+    print('Total number of epochs studied =', n_epochs)
+    epochs = []; psds = []
+    for i_ep in range(n_epochs):
+        # Segment data into epochs
+        start, stop = int(i_ep * np.floor(T_ep * Fs)), int((i_ep + 1) * np.floor(T_ep * Fs))
+        ep_data = raw_data[:, start:stop]
+        epochs.append(ep_data)
+        # Compute PSDs
+        psds_ep, freqs = _computeFFTPSD(ep_data, Fs, detrend=True, window='cosine',
+                                       plot_psd_mean=plot_psd_epochs)
+        psds.append(psds_ep * 1e12) # in μV^2
+    print('Shape of epochs =', np.shape(epochs))
+    print('Shape of psds =', np.shape(psds))
+    psds_mean_epochs = np.mean(psds, axis=0) # Average psds over epochs
+
+    return psds_mean_epochs, freqs
+
+def psd_M2(params, raw, plot_psd_epochs=None):
+    '''
+    Rangaswamy2002
+    The referencing is changed from monopolar to bipolar. Fourier transform is
+    applied to overlapping epochs (overlap = 50%) after Hamming windowing. The
+    resulting power spectral densities are averaged across epochs.
+    '''
+    # Unpack parameters
+    T_max = params['T_max']
+    print('A total of %d seconds of raw EEG data has been studied.' % T_max)
+    Fs = params['sfreq']
+    print('Fs =', Fs)
+    T_ep = params['size_epoch']
+    print('Each epoch has %0.1f seconds of EEG data.' % T_ep)
+    times = params['epoch_times']
+    print('Shape of times =', np.shape(times))
+
+    # Make a copy of raw data and delete projector for Average EEG reference
+    raw = raw.copy()
+    raw.del_proj(0) # delete Average EEG reference
+
+    # Rename EEG channels
+    channel_renaming_dict = {name: name.replace('EEG 0', '') for name in raw.ch_names}
+    _ = raw.rename_channels(channel_renaming_dict) # happens in-place
+
+    # Get anodes and cathodes of bipolar montage
+    bipolar_cnx = params['bipolar_connections']
+    anode = []; cathode = []
+    for a, c in bipolar_cnx.values():
+        anode.append(a); cathode.append(c)
+
+    # Set referencing as bipolar
+    raw.load_data()
+    raw_bip_ref = mne.set_bipolar_reference(raw, anode=anode, cathode=cathode)
+
+    # Get raw data
+    raw_data = raw_bip_ref.get_data(picks='eeg')
+    raw_data = raw_data[:, :int(Fs * T_max)]
+    print('Shape of raw_data =', np.shape(raw_data))
+
+    # Data segmentation and PSD calculation
+    n_win = int(np.floor(T_max / T_ep))
+    n_epochs = int(1 + 2 * (n_win - 1))
+    print('Total number of epochs studied =', n_epochs)
+    epochs = []; psds = []
+    for i_ep in range(n_epochs):
+        # Segment data into epochs
+        start, stop = int(i_ep / 2 * np.floor(T_ep*Fs)), int((i_ep / 2 + 1) * np.floor(T_ep*Fs))
+        ep_data = raw_data[:, start:stop]
+        epochs.append(ep_data)
+        # Compute PSDs
+        psds_ep, freqs = _computeFFTPSD(ep_data, Fs, detrend=False, window='hamming',
+                                           plot_psd_mean=plot_psd_epochs)
+        psds.append(psds_ep * 1e12) # in μV^2
+    print('Shape of epochs =', np.shape(epochs))
+    print('Shape of psds =', np.shape(psds))
+    psds_mean_epochs = np.mean(psds, axis=0) # Average psds over epochs
+
+    return psds_mean_epochs, freqs
+
+def psd_M3(params, raw, plot_psd_epochs=None):
+    '''
+    Winterer1998
+    EEG epochs are submitted to a Fast Fourier Transform which computes the power spectral
+    densities (PSDs) of the EEG. The PSDs are then averaged over epochs.
+    '''
+    # Unpack parameters
+    T_max = params['T_max']
+    print('A total of %d seconds of raw EEG data has been studied.' % T_max)
+    Fs = params['sfreq']
+    print('Fs =', Fs)
+    T_ep = params['size_epoch']
+    print('Each epoch has %0.1f seconds of EEG data.' % T_ep)
+    times = params['epoch_times']
+    print('Shape of times =', np.shape(times))
+
+    # Get raw data
+    raw = raw.copy()
+    raw.del_proj(0) # Delete Average EEG reference
+    raw_data = raw.get_data(picks='eeg')
+    raw_data = raw_data[:, :int(Fs * T_max)]
+    print('Shape of raw_data =', np.shape(raw_data))
+
+    # Data segmentation and PSD calculation
+    n_epochs = int(np.floor(T_max / T_ep))
+    print('Total number of epochs studied =', n_epochs)
+    epochs = []; psds = []
+    for i_ep in range(n_epochs):
+        # Segment data into epochs
+        start, stop = int(i_ep * np.floor(T_ep * Fs)), int((i_ep + 1) * np.floor(T_ep * Fs))
+        ep_data = raw_data[:, start:stop]
+        epochs.append(ep_data)
+        # Compute PSDs
+        psds_ep, freqs = _computeFFTPSD(ep_data, Fs, detrend=False, window=False,
+                                         plot_psd_mean=plot_psd_epochs)
+        psds.append(psds_ep * 1e12) # in μV^2
+    print('Shape of epochs =', np.shape(epochs))
+    print('Shape of psds =', np.shape(psds))
+    psds_mean_epochs = np.mean(psds, axis=0) # Average psds over epochs
+
+    return psds_mean_epochs, freqs
+
+def psd_M4(params, raw, plot_psd_epochs=None):
+    '''
+    Saletu-Zyhlarz2004
+    EEG epochs are submitted to a Fast Fourier Transform which computes the power spectral
+    densities (PSDs) of the EEG. The PSDs are then averaged over epochs.
+    '''
+    # Unpack parameters
+    T_max = params['T_max']
+    print('A total of %d seconds of raw EEG data has been studied.' % T_max)
+    Fs = params['sfreq']
+    print('Fs =', Fs)
+    T_ep = params['size_epoch']
+    print('Each epoch has %0.1f seconds of EEG data.' % T_ep)
+    times = params['epoch_times']
+    print('Shape of times =', np.shape(times))
+
+    # Get raw data
+    raw = raw.copy()
+    raw.del_proj(0) # Delete Average EEG reference
+    raw_data = raw.get_data(picks='eeg')
+    raw_data = raw_data[:, :int(Fs * T_max)]
+    print('Shape of raw_data =', np.shape(raw_data))
+
+    # Data segmentation and PSD calculation
+    n_epochs = int(np.floor(T_max / T_ep))
+    print('Total number of epochs studied =', n_epochs)
+    epochs = []; psds = []
+    for i_ep in range(n_epochs):
+        # Segment data into epochs
+        start, stop = int(i_ep * np.floor(T_ep * Fs)), int((i_ep + 1) * np.floor(T_ep * Fs))
+        ep_data = raw_data[:, start:stop]
+        epochs.append(ep_data)
+        # Compute PSDs
+        psds_ep, freqs = _computeFFTPSD(ep_data, Fs, detrend=False, window=False,
+                                         plot_psd_mean=plot_psd_epochs)
+        psds.append(psds_ep * 1e12) # in μV^2
+    print('Shape of epochs =', np.shape(epochs))
+    print('Shape of psds =', np.shape(psds))
+    psds_mean_epochs = np.mean(psds, axis=0) # Average psds over epochs
+
+    return psds_mean_epochs, freqs
+
+def psd_M5(params, raw, plot_psd_epochs=None):
+    '''
+    Default2022
+    All EEG epochs are detrended and cosine tapered. EEG epochs with zero-
+    overlap are submitted to a Fast Fourier Transform which computes the power
+    spectral density (in μV^2/Hz) of the EEG. The power spectral densities are
+    then averaged over epochs.
+    '''
+    # Unpack parameters
+    T_max = params['T_max']
+    print('A total of %d seconds of raw EEG data has been studied.' % T_max)
+    Fs = params['sfreq']
+    print('Fs =', Fs)
+    T_ep = params['size_epoch']
+    print('Each epoch has %0.1f seconds of EEG data.' % T_ep)
+    times = params['epoch_times']
+    print('Shape of times =', np.shape(times))
+
+    # Get raw data
+    raw = raw.copy()
+    raw.del_proj(0) # delete Average EEG reference
+    raw_data = raw.get_data(picks='eeg')
+    raw_data = raw_data[:, :int(Fs * T_max)]
+    print('Shape of raw_data =', np.shape(raw_data))
+
+    # Data segmentation and PSD calculation
+    n_epochs = int(np.floor(T_max / T_ep))
+    print('Total number of epochs studied =', n_epochs)
+    epochs = []; psds = []
+    for i_ep in range(n_epochs):
+        # Segment data into epochs
+        start, stop = int(i_ep * np.floor(T_ep * Fs)), int((i_ep + 1) * np.floor(T_ep * Fs))
+        ep_data = raw_data[:, start:stop]
+        epochs.append(ep_data)
+        # Compute PSDs
+        psds_ep, freqs = _computeFFTPSD(ep_data, Fs, detrend=True, window='cosine',
+                                       plot_psd_mean=plot_psd_epochs)
+        psds.append(psds_ep * 1e12) # in μV^2
+    print('Shape of epochs =', np.shape(epochs))
+    print('Shape of psds =', np.shape(psds))
+    psds_mean_epochs = np.mean(psds, axis=0) # Average psds over epochs
+
+    return psds_mean_epochs, freqs
+
+def load_bipolar_connections(fname_bipolar_connections):
+    with open(fname_bipolar_connections, 'rb') as f:
+        return pickle.load(f)
+
+def _computeFFTPSD(X, Fs, times=None, detrend=None, baseline=None, window=None,
+                  plot_psd_mean=None):
+    '''
+    Compute the Fourier power spectral density of the N-channel block `X`
+    [nChannels, nSamples] sampled at frequency `Fs`.
+    '''
+
+    # Detrend data
+    if detrend and detrend is not None:
+        X = __detrend(X)
+
+    # Baseline correction
+    if baseline is not None:
+        # Baseline correction
+        X = __baseline_correction(X, times, baseline=baseline)
+        # Discard baseline data
+        qmin = np.where(times <= 0)[0]
+        qmin = int(qmin[-1]) + 1
+        X = X[:, qmin:]
+
+    # Window size
+    n = np.size(X, 1)
+
+    # Windowing
+    if window and window is not None:
+        if window == 'hamming':
+            w = np.hamming(n)
+        elif window == 'hanning':
+            w = np.hanning(n)
+        elif window == 'cosine':
+            w = signal.tukey(n)
+        X = w * X
+
+    # FFT using Numpy
+    Xhat = np.fft.rfft(X)
+    psd = np.real(Xhat * np.conj(Xhat)) / n # FFT PSD
+    freq = Fs / n * np.arange(n)
+    k = np.arange(1, np.floor(n / 2), dtype='int') # Only use the first half of frequencies
+    freq = freq[k]; psd = psd[:, k];
+
+
+    # Plot power spectrum
+    if plot_psd_mean and plot_psd_mean is not None:
+        psd_mean = np.mean(psd, axis=0)
+        plt.figure()
+        plt.plot(freq, psd_mean)
+        plt.xlabel('Frequency [Hz]')
+        plt.ylabel('Power Spectral Density [V2/Hz]')
+        plt.show()
+
+    return psd, freq
+
+def _integrate_psd(psd, f, band):
+    '''
+    Integrate power spectral densities over desired frequency band.
+    '''
+    inds = [(np.abs(f - band[0])).argmin(), (np.abs(f - band[1])).argmin()]
+    return np.trapz(psd[inds], f[inds])
+
+def __detrend(data, detrend_type='linear', bp=0):
+    """
+    Remove linear trend along second axis from 2D array-like object `data`.
+                            [Adapted from MNE-Python]
+    """
+    axis = -1
+    if detrend_type not in ['linear', 'l', 'constant', 'c']:
+        raise ValueError("Trend type must be 'linear' or 'constant'.")
+    data = np.asarray(data)
+    if detrend_type in ['constant', 'c']:
+        data_detrended = data - np.mean(data, axis, keepdims=True)
+        return data_detrended
+    else:
+        dshape = data.shape
+        N = dshape[axis]
+        bp = np.sort(np.unique(np.r_[0, bp, N]))
+        if np.any(bp > N):
+            raise ValueError("Breakpoints must be less than length "
+                             "of data along given axis.")
+        Nreg = len(bp) - 1
+        # Find leastsq fit and remove it for each piece
+        newdata = data.T
+        for m in range(Nreg):
+            Npts = bp[m + 1] - bp[m]
+            A = np.ones((Npts, 2))
+            A[:, 0] = np.arange(1, Npts+1) / Npts
+            sl = slice(bp[m], bp[m+1])
+            coef, _, _, _ = np.linalg.lstsq(A, newdata[sl], rcond=None)
+            newdata[sl] = newdata[sl] - np.dot(A, coef)
+        data_detrended = newdata.T
+
+        return data_detrended
+
+def __baseline_correction(data, times, baseline=(None, 0)):
+    '''
+    Apply baseline correction to `data`.
+                            [Adapted from MNE-Python]
+    '''
+    bmin, bmax = baseline
+    if bmin is None:
+        imin = 0
+    else:
+        imin = np.where(times >= bmin)[0]
+        imin = int(imin[0])
+    if bmax is None:
+        imax = len(times)
+    else:
+        imax = np.where(times <= bmax)[0]
+        imax = int(imax[-1]) + 1
+
+    baseline = np.mean(data[..., imin:imax], axis=-1, keepdims=True)
+    corrected_data = data - baseline
+
+    return corrected_data
